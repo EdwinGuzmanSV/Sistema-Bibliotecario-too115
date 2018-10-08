5f5f54fa7c057aeb60406bba3ce94b6a5a321c80
fernando <jferhernandez1996@gmail.com>
2018-10-07 22:49:01 -0600

    Proyectos

---
+++ Administradores/Administradores/Admin.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Admin]
+(
+    [IDAdministrador]      INT           IDENTITY (1, 1) NOT NULL,
+	[Dui]      NVARCHAR (50) NULL,
+    [Nombre]       NVARCHAR (50) NULL,
+    [Apellido]      NVARCHAR (50) NULL,
+	[FechaDeNacimiento] DATETIME      NULL,
+	[Genero]      NVARCHAR (50) NULL,
+	[Direccion]      NVARCHAR (50) NULL,
+	[Telefono]      NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([IDAdministrador] ASC)
+)
---
+++ GestionAdministradores/GestionAdministradores/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionAdministradores.Startup))]
+namespace GestionAdministradores
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
